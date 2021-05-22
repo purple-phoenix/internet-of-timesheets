f6dfa471ef0933b2ccfbaaa69ced6a60a97ba4ff
Matt McCarthy <matttmccarthy66@gmail.com>
2021-05-22 15:07:34 -0400

    Add main program skeleton

--- lichess_timesheet.py
+++ lichess_timesheet.py
@@ -1,12 +1,15 @@
 """Module for auto logging time spent on lichess"""
 
-from typing import Tuple
+from typing import Tuple, Optional
 from datetime import datetime, timezone
+
+from time import sleep
 
 import lichess.api
 
 LichessID = str
 ActivityInterval = Tuple[datetime, datetime]
+Game = str
 
 
 def get_most_recent_activity_interval(user_id: LichessID) -> ActivityInterval:
@@ -18,3 +21,70 @@ def get_most_recent_activity_interval(user_id: LichessID) -> ActivityInterval:
     start_dt = datetime.fromtimestamp(int(start_epoch)/1000, timezone.utc)
     end_dt = datetime.fromtimestamp(int(end_epoch)/1000, timezone.utc)
     return start_dt, end_dt
+
+
+POLL_RATE = 5
+GAME_TIMEOUT = 5
+
+
+def update_chess_timesheet(maybe_last_latest_game: Optional[Game]):
+    """Look for new game,if tracking wait and check again,if not start tracking
+    If no new game within timeout stop tracking at end of last game"""
+    latest_game = get_latest_game()
+    if is_tracking_lichess():
+        sleep(POLL_RATE)
+        update_chess_timesheet(latest_game)
+    should_stop_tracking, should_start_tracking = \
+        no_new_game(latest_game, maybe_last_latest_game)
+    if should_stop_tracking:
+        stop_tracking()
+    elif should_start_tracking:
+        start_tracking(latest_game)
+
+
+StopTracker = bool
+StartNewTracker = bool
+
+TransitionState = Tuple[StopTracker, StartNewTracker]
+
+STOPTRACKING = True
+DONTSTOPTRACKING = False
+STARTTRACKING = True
+DONTSTARTTRACKING = False
+
+
+def no_new_game(latest_game: Game,
+                maybe_last_latest_game: Optional[Game]) -> TransitionState:
+    """Determines if no new game has been
+    started since the last latest game"""
+    # If there was no previous game, then we have not recursed
+    # latest_game is not new if it is the first tracked game
+    if maybe_last_latest_game is None:
+        return DONTSTOPTRACKING, STARTTRACKING
+    last_latest_game: Game = maybe_last_latest_game
+    # No new game has been started
+    if latest_game == last_latest_game:
+        return STOPTRACKING, DONTSTARTTRACKING
+    return new_game_passed_timeout(latest_game, last_latest_game)
+
+
+def new_game_passed_timeout(latest_game: Game, last_latest_game: Game) -> TransitionState:
+    """Determines if the latest_game is beyond
+    the threshold of a new tracking event"""
+
+
+def is_tracking_lichess() -> bool:
+    """Determines if Toggl is currently tracking Lichess"""
+
+
+def start_tracking(game: Game) -> None:
+    """Starts tracking Lichess on Toggl"""
+
+
+def stop_tracking() -> None:
+    """Stop tracking Lichess on Toggl"""
+
+
+def get_latest_game() -> Game:
+    """Gets the latest game"""
+    return ""
--- test_lichess_timesheet.py
+++ test_lichess_timesheet.py
@@ -17,7 +17,18 @@ def test_get_most_recent_activity_interval(matt_lichess_id):
     """Tests whether most recent activity interval timestamps can be found"""
     activity_interval: MUT.ActivityInterval =\
         MUT.get_most_recent_activity_interval(matt_lichess_id)
-    current_dt = datetime.now(timezone.utc)
     start_interval = activity_interval[0]
     end_interval = activity_interval[1]
     assert end_interval > start_interval
+
+
+@pytest.mark.dev_test
+def test_is_tracking_lichess():
+    """Tests whether script can determine
+    if Toggl is currently tracking Lichess"""
+    assert not MUT.is_tracking_lichess()
+
+
+@pytest.mark.dev_test
+def test_get_latest_game():
+    """Tests if latest game can be captured"""
